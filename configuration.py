7f17de3554ed2392c17ea17d646d560ed6c8ba91
AdmorCum1337 <chistofatk@gmail.com>
2023-09-15 10:16:13 +0300

    first push

---
+++ configuration.py
@@ -0,0 +1,4 @@
+# Кирилл Чистофат, 8-я когорта — Финальный проект. Инженер по тестированию плюс
+URL_SERVICE = "https://1c0e3d40-9c8c-425f-803d-85eace5dbb7d.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_INFO = "/api/v1/orders/track"
